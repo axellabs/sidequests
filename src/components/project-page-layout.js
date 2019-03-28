986a644e0765842dac7489f94e30f33cabf80f75
Ax <ax01010@mail.com>
2019-03-28 00:04:16 -0400

    fixed rendering method of the individual project pages, each page's mdx loads individually

--- src/components/project-page-layout.js
+++ src/components/project-page-layout.js
@@ -1,31 +1,30 @@
 import React from 'react'
-import { StaticQuery, graphql } from 'gatsby'
+import { graphql } from 'gatsby'
 import MDXRenderer from 'gatsby-mdx/mdx-renderer'
 
 import Layout from './layout'
 
-const ProjectPageLayout = () => (
-  <StaticQuery
-    query={graphql`
-      query BlogPostQuery($id: String) {
-        mdx(id: { eq: $id }) {
-          id
-          frontmatter {
-            title
-          }
-          code {
-            body
-          }
-        }
+function ProjectPageLayout({ data: { mdx } }) {
+  return (
+    <Layout>
+      <h1>{mdx.frontmatter.title}</h1>
+      <MDXRenderer>{mdx.code.body}</MDXRenderer>
+    </Layout>
+  )
+}
+
+export const pageQuery = graphql`
+  query BlogPostQuery($id: String) {
+    mdx(id: { eq: $id }) {
+      id
+      frontmatter {
+        title
       }
-    `}
-    render={data => (
-      <Layout>
-        <h1>{data.mdx.frontmatter.title}</h1>
-        <MDXRenderer>{data.mdx.code.body}</MDXRenderer>
-      </Layout>
-    )}
-  />
-)
+      code {
+        body
+      }
+    }
+  }
+`
 
 export default ProjectPageLayout
