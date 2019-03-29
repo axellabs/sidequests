0f3893ae91c21aaaceff42b0385e4f7fb80d5b64
Axel Simms <mail@axelsimms.com>
2019-03-29 11:34:55 -0400

    added active states to the sidebar items

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 import { Link, StaticQuery, graphql } from 'gatsby'
+import { Location } from '@reach/router'
 
 import fire from '../assets/icons/fire-light.svg'
 import spinner from '../assets/icons/spinner-light.svg'
 import book from '../assets/icons/book-light.svg'
 import star from '../assets/icons/star-light.svg'
 import home from '../assets/icons/home-light.svg'
+
+import fireDark from '../assets/icons/fire.svg'
+import spinnerDark from '../assets/icons/spinner.svg'
+import bookDark from '../assets/icons/book-dark.svg'
+import starDark from '../assets/icons/star.svg'
+import homeDark from '../assets/icons/home.svg'
 
 const Header = styled.p`
   font-size: 45px;
@@ -34,24 +41,34 @@ const StyledLinkIcon = styled.img`
 const StyledLink = styled(Link).attrs({
   alt: 'project page icon',
 })`
-  color: black;
+  padding: 10px 0;
+  width: 100%;
+  color: ${props => (props.active === 'true' ? 'white' : 'black')};
+  background-color: ${props => (props.active === 'true' ? 'black' : 'white')};
   text-decoration: none;
   display: flex;
   p {
     font-size: 16px;
-    color: black;
+    color: ${props => (props.active === 'true' ? 'white' : 'black')};
     text-decoration: none;
+    margin-bottom: 0px;
   }
 `
 
 const StyledText = styled.p`
   font-size: 16px;
   color: black;
+  margin-bottom: 0px;
 `
 
 const ProjectText = props => (
   <div
-    style={{ display: 'flex', cursor: 'pointer', zIndex: 10 }}
+    style={{
+      display: 'flex',
+      cursor: 'pointer',
+      zIndex: 10,
+      padding: '10px 0',
+    }}
     onClick={props.onClick}
   >
     <StyledLinkIcon src={book} />
@@ -60,18 +77,29 @@ const ProjectText = props => (
 )
 
 const SideBarLink = props => (
-  <StyledLink to={props.to}>
-    <StyledLinkIcon src={props.src} />
+  <StyledLink to={props.to} active={props.active} style={props.style}>
+    <StyledLinkIcon
+      src={props.active === 'true' ? props.activeSrc : props.src}
+    />
     <p>{props.children}</p>
   </StyledLink>
 )
 
-const ParentLink = props => <SideBarLink {...props} src={home} />
+const ParentLink = props => (
+  <SideBarLink {...props} src={home} activeSrc={homeDark} />
+)
 
-const ChildLink = props => <SideBarLink {...props} src={star} />
+const ChildLink = props => (
+  <SideBarLink
+    {...props}
+    active={(props.to === props.location).toString()}
+    src={star}
+    activeSrc={starDark}
+  />
+)
 
 const StyledAccordion = styled.div`
-  height: ${props => (props.open ? 'auto' : '50px')};
+  height: ${props => (props.open ? 'auto' : '45px')};
   overflow-y: hidden;
 `
 
@@ -90,19 +118,29 @@ class Accordion extends Component {
   }
 
   render() {
-    const { parent, subposts } = this.props
+    const { parent, subposts, location } = this.props
     const { open } = this.state
     return (
       <StyledAccordion open={open}>
         <ProjectText onClick={this.toggleAccordion} open={open}>
           {parent.frontmatter.title}
         </ProjectText>
-        <div style={{ marginLeft: '45px' }}>
-          <ParentLink to={parent.fields.slug} open={open}>
+        <div>
+          <ParentLink
+            to={parent.fields.slug}
+            open={open}
+            active={(parent.fields.slug === location).toString()}
+            style={{ paddingLeft: '45px' }}
+          >
             {parent.frontmatter.title}
           </ParentLink>
           {subposts.map(({ node: post }) => (
-            <ChildLink to={post.fields.slug} key={post.fields.slug}>
+            <ChildLink
+              to={post.fields.slug}
+              key={post.fields.slug}
+              location={location}
+              style={{ paddingLeft: '45px' }}
+            >
               {post.frontmatter.title}
             </ChildLink>
           ))}
@@ -112,7 +150,7 @@ class Accordion extends Component {
   }
 }
 
-const SideBar = () => (
+const SideBar = props => (
   <StaticQuery
     query={graphql`
       query sideBar {
@@ -160,19 +198,38 @@ const SideBar = () => (
           <Header>
             <StyledLink to="/">{data.site.siteMetadata.title}</StyledLink>
           </Header>
-          <SideBarLink to="/" src={fire}>
-            Favorites
-          </SideBarLink>
-          <SideBarLink to="/brainstorm/" src={spinner}>
-            Running Projects
-          </SideBarLink>
-          {hierarchalPosts.map(accordionData => (
-            <Accordion
-              parent={accordionData.parent}
-              subposts={accordionData.subnodes}
-              key={accordionData.parent.frontmatter.title}
-            />
-          ))}
+          <Location>
+            {({ location }) => {
+              return (
+                <>
+                  <SideBarLink
+                    to="/"
+                    src={fire}
+                    activeSrc={fireDark}
+                    active={('/' === location.pathname).toString()}
+                  >
+                    Favorites
+                  </SideBarLink>
+                  <SideBarLink
+                    to="/brainstorm/"
+                    src={spinner}
+                    activeSrc={spinnerDark}
+                    active={('/brainstorm/' === location.pathname).toString()}
+                  >
+                    Running Projects
+                  </SideBarLink>
+                  {hierarchalPosts.map(accordionData => (
+                    <Accordion
+                      parent={accordionData.parent}
+                      subposts={accordionData.subnodes}
+                      key={accordionData.parent.frontmatter.title}
+                      location={location.pathname}
+                    />
+                  ))}
+                </>
+              )
+            }}
+          </Location>
         </StyledSideBar>
       )
     }}
